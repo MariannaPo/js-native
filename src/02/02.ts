fcb85d6837f905cf4f451195b328217687ba840e
MariannaPo <mapodmasko@gmail.com>
2022-09-15 14:46:15 +0300

    02/test-city

---
+++ src/02/02.ts
@@ -0,0 +1,52 @@
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    name: 'Marianna',
+    age: 23,
+    isActive: false,
+    address: {
+        streetTitle: 'Pleseckaya 6 ',
+        city: {
+            title: "SPb",
+            countryTitle: "Russia"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+             title: "HTML",
+        }  ,
+        {
+            id: 2,
+            title: "JS",
+        },
+        {
+            id: 3,
+            title: "REACT",
+        }
+    ]
+}
+console.log(student.technologies[2].title)
